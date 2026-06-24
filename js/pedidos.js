66b38b071503224af3d1ff104fca3c2b524dd7d7
rodrigoarvisu <rodrigo_arvisu@hotmail.com>
2026-06-23 21:20:15 -0600

    Guardar pedidos en la base de datos

--- js/pedidos.js
+++ js/pedidos.js
@@ -18,3 +18,39 @@ function agregarALista(platillo, id) {
     `;
 }
 
+
+const formularioPedido = document.getElementById("formPedido");
+
+formularioPedido.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const pedidoNuevo = {
+        platilloId: formularioPedido.platillo.value,
+        nombre: formularioPedido.nombre.value,
+        direccion: formularioPedido.direccion.value
+    };
+
+    db.collection("pedidos").add(pedidoNuevo)
+        .then(() => {
+            Swal.fire({
+                title: "¡Muy bien!",
+                text: "Pedido guardado correctamente",
+                icon: "success",
+                confirmButtonText: "Aceptar"
+            });
+
+            formularioPedido.reset();
+            M.FormSelect.init(document.querySelectorAll("select"));
+        })
+        .catch((error) => {
+            console.error(error);
+
+            Swal.fire({
+                title: "¡Error!",
+                text: "No se pudo guardar el pedido",
+                icon: "error",
+                confirmButtonText: "Aceptar"
+            });
+        });
+});
+
